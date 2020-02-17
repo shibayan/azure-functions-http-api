afbb2ff74f02e1a40b67531fbd3573e7d69d3274
Tatsuro Shibamura <me@shibayan.jp>
2020-02-18 00:13:28 +0900

    Adding new function

---
+++ HttpApi/Function2.cs
@@ -0,0 +1,29 @@
+using System;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HttpApi
+{
+    public class Function2 : HttpFunctionBase
+    {
+        public Function2(IHttpContextAccessor httpContextAccessor)
+            : base(httpContextAccessor)
+        {
+        }
+
+        [FunctionName("Function2")]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get")]
+            HttpRequest req,
+            ILogger log)
+        {
+            Response.Headers.Add("Cache-Control", "no-cache");
+
+            return Ok($"Now: {DateTime.Now}");
+        }
+    }
+}
