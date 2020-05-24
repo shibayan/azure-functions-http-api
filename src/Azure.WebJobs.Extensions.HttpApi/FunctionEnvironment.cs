2335c6790d861efdd9faa2f994859a2d055d31cb
Tatsuro Shibamura <me@shibayan.jp>
2020-05-24 19:31:45 +0900

    Support Linux functions

--- src/Azure.WebJobs.Extensions.HttpApi/FunctionEnvironment.cs
+++ src/Azure.WebJobs.Extensions.HttpApi/FunctionEnvironment.cs
@@ -4,8 +4,10 @@ namespace Azure.WebJobs.Extensions.HttpApi
 {
     internal static class FunctionEnvironment
     {
-        public static bool IsAvailable => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"));
+        public static bool IsAvailable
+            => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"));
 
-        public static string RootPath => IsAvailable ? Environment.ExpandEnvironmentVariables(@"%HOME%\site\wwwroot") : Environment.CurrentDirectory;
+        public static string RootPath
+            => IsAvailable ? Environment.ExpandEnvironmentVariables($"%HOME%/site/wwwroot") : Environment.CurrentDirectory;
     }
 }
