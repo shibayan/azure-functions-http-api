57152ec9be733ef5b23063a275a0f805cf5a031b
Tatsuro Shibamura <me@shibayan.jp>
2021-10-28 17:13:59 +0900

    Adding `ProxyResultBase` for shared code

--- src/Azure.WebJobs.Extensions.HttpApi/Proxy/ProxyResult.cs
+++ src/Azure.WebJobs.Extensions.HttpApi/Proxy/ProxyResult.cs
@@ -1,59 +1,21 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Mvc;
 
 namespace Azure.WebJobs.Extensions.HttpApi.Proxy
 {
-    internal class ProxyResult : IActionResult
+    internal class ProxyResult : ProxyResultBase
     {
         public ProxyResult(string backendUri)
+            : base(backendUri)
         {
-            _backendUri = backendUri;
         }
-
-        private readonly string _backendUri;
-
-        private static readonly Regex _templateRegex = new Regex(@"\{([^\{\}]+)\}", RegexOptions.Compiled);
-
-        public ProxyInvoker ProxyInvoker { get; set; }
 
         public Action<HttpRequestMessage> Before { get; set; }
 
         public Action<HttpResponseMessage> After { get; set; }
 
-        public async Task ExecuteResultAsync(ActionContext context)
-        {
-            try
-            {
-                var backendUri = MakeBackendUri(context);
+        protected override void BeforeSend(HttpRequestMessage request) => Before?.Invoke(request);
 
-                await ProxyInvoker.SendAsync(backendUri, context.HttpContext, Before, After);
-            }
-            catch
-            {
-                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            }
-        }
-
-        private string MakeBackendUri(ActionContext context)
-        {
-            var routeValues = context.RouteData.Values;
-
-            var backend = _templateRegex.Replace(_backendUri, match =>
-            {
-                if (routeValues.TryGetValue(match.Groups[1].Value, out var value) && value != null)
-                {
-                    return value.ToString();
-                }
-
-                return "";
-            });
-
-            return backend;
-        }
+        protected override void AfterSend(HttpResponseMessage response) => After?.Invoke(response);
     }
 }
