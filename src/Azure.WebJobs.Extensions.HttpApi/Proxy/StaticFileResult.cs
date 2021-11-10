3bdfe5850052dd151191111dea475eb9927072a0
Tatsuro Shibamura <me@shibayan.jp>
2021-11-10 19:23:29 +0900

    Adding draft `StaticFileResult` impl

---
+++ src/Azure.WebJobs.Extensions.HttpApi/Proxy/StaticFileResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure.WebJobs.Extensions.HttpApi.Proxy
+{
+    internal class StaticFileResult : IActionResult
+    {
+        public string ContentRoot { get; set; }
+
+        public string DefaultFile { get; set; }
+
+        public string FallbackPath { get; set; }
+
+        public string FallbackExclude { get; set; }
+
+        private static readonly StaticFileResultExecutor _executor = new();
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return _executor.ExecuteAsync(context, this);
+        }
+    }
+}
---
+++ src/Azure.WebJobs.Extensions.HttpApi/Proxy/StaticFileResultExecutor.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Azure.WebJobs.Extensions.HttpApi.Proxy
+{
+    internal class StaticFileResultExecutor : IActionResultExecutor<StaticFileResult>
+    {
+        public Task ExecuteAsync(ActionContext context, StaticFileResult result)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
