97bd4c61c046f940d98d50846bedc84a90966e19
Tatsuro Shibamura <me@shibayan.jp>
2021-11-11 16:38:32 +0900

    Fixed directory root file access error (#25)

---
+++ samples/WebsiteSample/wwwroot/_nuxt/288fc1e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{224:function(e,n,t){"use strict";t.r(n);var l=t(43),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Test page")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ samples/WebsiteSample/wwwroot/_nuxt/e5fdf82.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{169:function(n,o,w){w(170),n.exports=w(171)}},[[169,6,1,7]]]);
