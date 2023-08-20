bef2f17986859438610cca5c9ff8ec48acc165c2
Tatsuro Shibamura <me@shibayan.jp>
2023-08-20 17:13:17 +0900

    Adding .NET Isolated worker support

---
+++ samples/IsolatedSample/HttpContextAccessorMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+
+namespace IsolatedSample;
+
+internal class HttpContextAccessorMiddleware : IFunctionsWorkerMiddleware
+{
+    public HttpContextAccessorMiddleware(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        _httpContextAccessor.HttpContext = context.GetHttpContext();
+
+        await next(context);
+    }
+}
---
+++ src/Azure.Functions.Worker.Extensions.HttpApi/Config/HttpApiBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Azure.Functions.Worker.Extensions.HttpApi.Internal;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Azure.Functions.Worker.Extensions.HttpApi.Config;
+
+public static class HttpApiBuilderExtensions
+{
+    public static IFunctionsWorkerApplicationBuilder AddHttpApi(this IFunctionsWorkerApplicationBuilder builder)
+    {
+        if (builder is null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        builder.Services.AddHttpContextAccessor();
+        builder.UseMiddleware<HttpContextAccessorMiddleware>();
+
+        return builder;
+    }
+}
