45f4ffb412dd8da9088901b91310b6b58d6e1fa4
Tatsuro Shibamura <me@shibayan.jp>
2025-10-06 12:30:00 +0900

    Support App Service and Static Web App client principal

---
+++ samples/IsolatedSample/Function2.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Functions.Worker.Extensions.HttpApi;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace IsolatedSample;
+
+public class Function2(IHttpContextAccessor httpContextAccessor, ILogger<Function2> logger)
+    : HttpFunctionBase(httpContextAccessor)
+{
+    [Function("Function2")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return Unauthorized();
+        }
+
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        return Ok($"Hello, {User.Identity.Name}");
+    }
+}
